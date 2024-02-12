146c1b62116e9a323c551ccba4f0c7cfa7f20548
Matheus <matheus.hpp135@gmail.com>
2024-02-12 17:29:51 -0300

    feat: 🎸 create a toggle function

--- lua/live-server.lua
+++ lua/live-server.lua
@@ -23,10 +23,25 @@ local function find_cached_dir(dir)
     return cur
 end
 
+local function is_running(dir)
+    local cached_dir = find_cached_dir(dir)
+    return cached_dir and job_cache[cached_dir]
+end
+
 M.config = {
     -- 8080 default is commonly used
     args = { '--port=5555' },
 }
+
+M.toggle = function()
+    local dir = vim.fn.expand '%:p:h'
+    local running = is_running(dir)
+    if not running then
+        M.start()
+        return
+    end
+    M.stop()
+end
 
 M.setup = function(user_config)
     M.config = vim.tbl_deep_extend('force', M.config, user_config or {})
@@ -41,13 +56,14 @@ M.setup = function(user_config)
 
     vim.api.nvim_create_user_command('LiveServerStart', M.start, {})
     vim.api.nvim_create_user_command('LiveServerStop', M.stop, {})
+    vim.api.nvim_create_user_command('LiveServerToggle', M.toggle, {})
 end
 
 M.start = function()
     local dir = vim.fn.expand '%:p:h'
-    local cached_dir = find_cached_dir(dir)
+    local running = is_running(dir)
 
-    if cached_dir then
+    if running then
         log('live-server already running', 'INFO')
         return
     end
@@ -82,11 +98,15 @@ end
 
 M.stop = function()
     local dir = vim.fn.expand '%:p:h'
-    local cached_dir = find_cached_dir(dir)
+    local running = is_running(dir)
 
-    if cached_dir then
-        vim.fn.jobstop(job_cache[cached_dir])
-        job_cache[cached_dir] = nil
+    if running then
+        local cached_dir = find_cached_dir(dir)
+        if cached_dir then
+            vim.fn.jobstop(job_cache[cached_dir])
+            job_cache[cached_dir] = nil
+            log('live-server stopped', 'INFO')
+        end
     end
 end
 
